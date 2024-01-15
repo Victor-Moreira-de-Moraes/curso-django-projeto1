5f62d3de296bcbc21e0626b1a2e96f93270de03f
Victor-Moreira-de-Moraes <victormoreira3202@gmail.com>
2024-01-15 00:08:42 -0300

    REMOVENDO CONTATO E SOBRE

--- recipes/urls.py
+++ recipes/urls.py
@@ -1,11 +1,9 @@
 from django.urls import path
-from recipes.views import contato, home, sobre
+from recipes.views import home
 
 # HTTP REQUEST <- HTTP RESPONSE
 
 # dominio/recipes/
 urlpatterns = [
     path('', home), # home
-    path('sobre/', sobre), # /sobre/
-    path('contato/', contato), # /contato/
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,4 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,11 +5,3 @@ def home(request):
     return render(request, 'recipes/home.html', context={
         'name': 'Victor Moreira',
     })
-
-# HTTP REQUEST
-def contato(request):
-    return HttpResponse('contato')
-    # RETURN HTTP Response
-
-def sobre(request):
-    return HttpResponse('sobre')
