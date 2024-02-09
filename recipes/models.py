5fbee6dd63cf5b6eb908ea0fb902e196b7ebef0f
Victor-Moreira-de-Moraes <victormoreira3202@gmail.com>
2024-02-09 14:55:24 -0300

    Models

--- recipes/models.py
+++ recipes/models.py
@@ -17,7 +17,6 @@ class Recipe(models.Model):
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
-    preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
@@ -28,6 +27,10 @@ class Recipe(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.SET_NULL, null=True
     )
+
+    def __str__(self):
+        return self.title
+    
 # EDITED
 # title description slug
 # preparation_time preparation_time_unit
