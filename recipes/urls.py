f7f1ee546f1ecf3e9cd41d91188abc08f07b7313
Victor-Moreira-de-Moraes <victormoreira3202@gmail.com>
2024-02-09 11:52:01 -0300

    Trabalhando com admin

--- recipes/urls.py
+++ recipes/urls.py
@@ -4,8 +4,10 @@ from . import views
 
 # HTTP REQUEST <- HTTP RESPONSE
 
+app_name = 'recipes'
+
 # dominio/recipes/
 urlpatterns = [
-    path('', views.home), 
-    path('recipes/<int:id>/', views.recipe), 
+    path('', views.home, name="home"), 
+    path('recipes/<int:id>/', views.recipe, name="recipe"), 
 ]
--- utils/recipes/abroba.py
+++ utils/recipes/abroba.py
@@ -14,12 +14,13 @@ fake = Faker('pt_BR')
 
 def make_recipe():
     return {
+        'id': fake.random_number(digits=2, fix_len=True),
         'title': fake.sentence(nb_words=6),
         'description': fake.sentence(nb_words=12),
         'preparation_time': fake.random_number(digits=2, fix_len=True),
         'preparation_time_unit': 'Minutos',
         'servings': fake.random_number(digits=2, fix_len=True),
-        'servings_unit': 'Porção',
+        'servings_unit': 'Porcao',
         'preparation_steps': fake.text(3000),
         'created_at': fake.date_time(),
         'author': {
@@ -38,3 +39,4 @@ def make_recipe():
 if __name__ == '__main__':
     from pprint import pprint
     pprint(make_recipe())
+    
