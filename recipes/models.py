4c9d59e76f14473bc03d8e8ba08f3b0061780d9e
Victor-Moreira-de-Moraes <victormoreira3202@gmail.com>
2024-02-12 22:11:56 -0300

    tistreza

--- recipes/models.py
+++ recipes/models.py
@@ -17,6 +17,7 @@ class Recipe(models.Model):
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
     preparation_steps = models.TextField()
+    preparation_steps_is_html = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
